a0c502000579938427d983cee8adbba031eb90b0
SebastianOrias <soriasb4@gmail.com>
2024-03-11 09:31:21 -0400

    RQ2

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -37,11 +37,6 @@
       </div>
     </nav>
 
-    <!-- Content Container -->
-    <div class="bg-white p-5">
-      <h1>Saludos!</h1>
-      <p>Bienvenido a nuestra página dedicada a los algoritmos.</p>
-    </div>
 
     <!-- Rounded Buttons -->
     <div class="rounded-buttons">
@@ -91,6 +86,10 @@
       </div>
     </div>
 
+      <!-- Texto debajo del carrusel -->
+      <div class="bg-white p-5">
+      <h2>Información sobre Grafos y Algoritmos</h2>
+    </div>
     <!-- Carrusel de Imágenes -->
     <div id="imageCarousel" class="carousel slide mt-4" data-bs-ride="carousel">
       <div class="carousel-inner">
@@ -99,7 +98,7 @@
             <img src="../assets/Grafos.jpg" class="d-block w-25" alt="Grafos">
             <div class="ms-3">
               <h2>¿Qué es un Grafo?</h2>
-              <p>Los grafos son estructuras discretas que constan de vértices y aristas que conectan entre si esos vértices.  Por lo tanto un grafo G costa de dos partes: 1)  Un conjunto V = V (G) cuyos elementos se denominan vértices, puntos o nodos de G.</p>
+              <p>Los grafos son estructuras discretas que constan de vértices y aristas que conectan entre sí esos vértices. Por lo tanto, un grafo G consta de dos partes: 1) Un conjunto V = V(G) cuyos elementos se denominan vértices, puntos o nodos de G.</p>
             </div>
           </div>
         </div>
@@ -108,7 +107,7 @@
             <img src="../assets/Algoritmo.jpg" class="d-block w-25" alt="Algoritmos">
             <div class="ms-3">
               <h2>¿Qué es un Algoritmo?</h2>
-              <p>Se llaman algoritmos el conjunto de instrucciones sistemáticas y previamente definidas que se utilizan para realizar una determinada tarea. Estas instrucciones están ordenadas y acotadas a manera de pasos a seguir para alcanzar un objetivo.</p>
+              <p>Se llaman algoritmos al conjunto de instrucciones sistemáticas y previamente definidas que se utilizan para realizar una determinada tarea. Estas instrucciones están ordenadas y acotadas a manera de pasos a seguir para alcanzar un objetivo.</p>
             </div>
           </div>
         </div>
@@ -131,22 +130,53 @@
         <span class="visually-hidden">Siguiente</span>
       </button>
     </div>
+
+    
+
+    <!-- Enlaces de videos de YouTube -->
+    <div class="mt-5">
+      <h2>Información extra </h2>
+      <h2>Videos Informativos</h2>
+      <div class="row">
+        <div class="col-md-4 mb-4">
+          <a href="https://www.youtube.com/watch?v=F5Xjpg0-NhM" target="_blank">
+            <img src="../assets/Video1.png" alt="Video 1" class="img-fluid">
+          </a>
+        </div>
+        <div class="col-md-4 mb-4">
+          <a href="https://www.youtube.com/watch?v=23pdz9VtIBo" target="_blank">
+            <img src="../assets/Video2.png" alt="Video 2" class="img-fluid">
+          </a>
+        </div>
+        <div class="col-md-4 mb-4">
+          <a href="https://www.youtube.com/watch?v=vnNFiNVy9KM" target="_blank">
+            <img src="../assets/Video3.png" alt="Video 3" class="img-fluid">
+          </a>
+        </div>
+      </div>
+    </div>
+
+    <!-- Enlaces a papers de Internet -->
+    <div class="md-4">
+      <h2>Papers Informativos</h2>
+      <div class="row">
+        <div class="col-md-4 mb-6">
+          <a href="http://www.cs.uns.edu.ar/~prf/teaching/AyC17/downloads/Teoria/Grafos-4x1.pdf" target="_blank"><img src="../assets/paper1.png" alt="Paper 1" class="img-fluid"></a>
+        </div>
+        <div class="col-md-4 mb-4">
+          <a href="https://repositorio.cmmedu.uchile.cl/Instructional%20design%20(of%20materiales%20or%20pedagogical%20models)./Herramientas%20para%20la%20formaci%C3%B3n%20de%20profesores%20de%20matem%C3%A1tica/01%20-%20GRAFOS%20Fundamentos%20y%20algoritmos.pdf" target="_blank"><img src="../assets/paper2.png" alt="Paper 2" class="img-fluid"></a>
+        </div>
+        <div class="col-md-4 mb-4">
+          <a href="https://www.dm.uba.ar/materias/investigacion_operativa/2011/2/capit_2.pdf" target="_blank"><img src="../assets/paper3.png" alt="Paper 3" class="img-fluid"></a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
-import { useAuthStore } from "../stores/auth";
-import { useRouter } from "vue-router";
-import UserlistService from "../services/userlistService";
-
 export default {
   setup() {
-    const authStore = useAuthStore();
-    const router = useRouter();
-    const helpCenterModal = ref(null);
-    const perfilModal = ref(null);
-
     const goEditor = () => {
       router.push("/editor");
     };
@@ -155,27 +185,11 @@ export default {
       helpCenterModal.value.toggle();
     };
 
-    const openSettings = () => {
-      // Redirige al perfil cuando se abre la configuración
-      router.push("/perfil");
-    };
-
     return {
-      authStore,
       goEditor,
-      openHelp,
-      openSettings,
-      helpCenterModal,
-      perfilModal,
+      openHelp
     };
-  },
-
-  mounted() {
-    const userlistService = new UserlistService();
-    console.log("Dashboard mounted, Listing users");
-    const usr_list = userlistService.getUsers();
-    console.log(usr_list);
-  },
+  }
 };
 </script>
 
@@ -196,13 +210,65 @@ export default {
 
   /* Estilo para el carrusel de imágenes */
   #imageCarousel img {
-    height: 250px; 
-    width: auto;   
+    height: 250px;
+    width: auto;
     object-fit: cover;
   }
 
   #imageCarousel h2,
   #imageCarousel p {
     color: black; /* Cambiado a negro */
+  }
+  
+/* Estilo para las flechas del carrusel */
+.carousel-control-prev-icon,
+.carousel-control-next-icon {
+  filter: invert(0.5) sepia(0%) saturate(0%) hue-rotate(259deg) brightness(107%) contrast(107%);
+}
+
+.carousel-control-prev,
+.carousel-control-next {
+  background-color: transparent;
+  border: none;
+  width: 50px; /* Ajusta el tamaño de las flechas */
+  height: 50px; /* Ajusta el tamaño de las flechas */
+  position: absolute;
+  top: 50%; /* Centra verticalmente */
+  transform: translateY(-50%); /* Centra verticalmente */
+}
+
+.carousel-control-prev {
+  left: 0; /* Ubica a la izquierda */
+}
+
+.carousel-control-next {
+  right: 0; /* Ubica a la derecha */
+}
+/* Estilo para las secciones de videos y papers */
+.video-section,
+  .paper-section {
+    padding: 20px; /* Añade espacio interno */
+    margin-top: 40px; /* Ajusta el tamaño del margen en la parte superior */
+    background-color: #f8f9fa; /* Fondo de color claro para resaltar */
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Sombra más suave */
+    border-radius: 10px; /* Borde redondeado */
+    border: 2px solid #007bff; /* Borde más grueso y de color azul */
+  }
+
+  /* Estilo para los enlaces de los videos */
+  .video-section a,
+  .paper-section a {
+    display: block; /* Convertir enlaces en bloques para ocupar todo el espacio del contenedor */
+    margin-bottom: 20px; /* Añade más espacio entre enlaces */
+    padding: 10px; /* Añade espacio interno al enlace */
+    border: 2px solid #007bff; /* Borde más grueso y de color azul */
+    border-radius: 5px; /* Borde redondeado */
+    transition: border-color 0.3s; /* Transición suave para el color del borde */
+  }
+
+  /* Cambiar el color del borde al pasar el ratón sobre los enlaces */
+  .video-section a:hover,
+  .paper-section a:hover {
+    border-color: #0056b3; /* Borde de color azul más oscuro al pasar el ratón */
   }
 </style>
